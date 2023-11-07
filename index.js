3973979f824f2e16cfc6c6d35f6188420722efcb
Lia <lia.cociorva@gmail.com>
2023-11-07 09:34:38 -0500

    3rd commit

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+const firebase = require("firebase/app");
+
+//unique project config info for firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyBbWzRna1YtOLptk5tu7ECpMNVVy9Bi-wg",
+    authDomain: "dynamic-web-exercise-5-8b917.firebaseapp.com",
+    projectId: "dynamic-web-exercise-5-8b917",
+    storageBucket: "dynamic-web-exercise-5-8b917.appspot.com",
+    messagingSenderId: "181549776622",
+    appId: "1:181549776622:web:481ad2414000a3a563bdff"
+  };
+
+firebase.initializeApp(firebaseConfig);
 
 const indexRoute = require("./routes/index");
 //const singlePostRoute = require("./routes/singlePost");
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,27 @@
 const express = require('express');
 const router = express.Router();
 
+//initialize firestore database
+const firestore = require ("firebase/firestore");
+//create a reference to the database
+const db = firestore.getFirestore();
+
 //Define index route - get all posts
 
 router.get("/", (req, res) => {
-    res.send("All Posts!")
+    const postsQuery = firestore.getDocs(firestore.collection(db, "posts"));
+    const postsArray = [];
+
+    postsQuery.then((response) => {
+        response.forEach((post) => {
+            console.log(post.data());
+            postsArray.push({ id: post.id, ...post.data()});
+        });
+        res.send(postsArray);
+    }).catch((error) => {
+    console/log(error);
+    return res.send (error);
+   });
 });
 
 module.exports = router;

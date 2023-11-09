04d3bcfaf2dce59f24d084918ec9579cdf2ce5ba
Lia <lia.cociorva@gmail.com>
2023-11-09 09:47:47 -0500

    singlepost

--- routes/singlePost.js
+++ routes/singlePost.js
@@ -1,1 +1,22 @@
-//get a single post
+const express = require('express');
+const router = express.Router();
+
+const firestore = require ("firebase/firestore");
+
+const db = firestore.getFirestore();
+
+const indexRoute = require("./index");
+
+router.get("/:id", (req, res) => {
+    const postID = req.params.id;
+    const postsQuery = firestore.getDoc(firestore.doc(db, "posts", postID));
+   
+    postsQuery.then((response) => {
+            res.send(response.data());
+    }).catch((error) => {
+    console/log(error);
+     res.send (error);
+   });
+});
+
+module.exports = router;
